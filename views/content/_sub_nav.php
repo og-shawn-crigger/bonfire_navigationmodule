fad2a521884aea7a07aea8e95eebe28d15c2a979
Sean Downey <sean@considerweb.com>
2011-08-01 10:05:40 +0100

    Changing the admin nav label

--- views/content/_sub_nav.php
+++ views/content/_sub_nav.php
@@ -1,4 +1,4 @@
 <div id="sub-nav" class="button-group">
-	<a href="<?php echo site_url(SITE_AREA.'/content/navigation') ?>" <?php echo $this->uri->segment(4) == '' ? 'class="current"' : '' ?> >Maintenance</a>
+	<a href="<?php echo site_url(SITE_AREA.'/content/navigation') ?>" <?php echo $this->uri->segment(4) == '' ? 'class="current"' : '' ?> >Links</a>
 	<a href="<?php echo site_url(SITE_AREA.'/content/navigation/groups') ?>" <?php echo $this->uri->segment(4) == 'groups' ? 'class="current"' : '' ?> >Groups</a>
 </div>
